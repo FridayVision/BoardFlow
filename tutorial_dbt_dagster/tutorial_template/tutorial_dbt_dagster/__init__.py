ed5ede22e119e267b547e351106f45d73de8d071
derinben <derinben@healint.com>
2023-08-02 21:10:39 +0800

    added dagster tutorial files

---
+++ tutorial_dbt_dagster/tutorial_template/tutorial_dbt_dagster/__init__.py
@@ -0,0 +1,26 @@
+import os
+
+from dagster_dbt import DbtCliClientResource
+from tutorial_dbt_dagster import assets
+from tutorial_dbt_dagster.assets import DBT_PROFILES, DBT_PROJECT_PATH
+
+from dagster_duckdb_pandas import duckdb_pandas_io_manager
+
+from dagster import Definitions, load_assets_from_modules
+
+
+resources = {
+    "dbt": DbtCliClientResource(
+        project_dir=DBT_PROJECT_PATH,
+        profiles_dir=DBT_PROFILES,
+    ),
+    "io_manager": duckdb_pandas_io_manager.configured(
+        {"database": os.path.join(DBT_PROJECT_PATH, "tutorial.duckdb")}
+    ),
+}
+
+defs = Definitions(assets=load_assets_from_modules([assets]), resources=resources)
+
+
+
+
