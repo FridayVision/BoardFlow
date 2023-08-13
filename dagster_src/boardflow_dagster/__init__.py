289c47e56c59cd69f870661ebe6ec68f9e360de3
derinben <derinben@healint.com>
2023-08-13 20:51:05 +0800

    remove commented out code

--- dagster_src/boardflow_dagster/__init__.py
+++ dagster_src/boardflow_dagster/__init__.py
@@ -4,8 +4,7 @@ from .assets import bgg_data
 bgg_data_assets = load_assets_from_package_module(
     package_module=bgg_data
 )
-# all_assets = load_assets_from_modules([bgg_data_pull,test_asset])
-# all_assets = load_assets_from_package_module()
+
 
 defs = Definitions(
     assets=bgg_data_assets,
